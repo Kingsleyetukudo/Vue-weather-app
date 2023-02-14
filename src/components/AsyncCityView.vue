c5c95dc2300bcca61a34fd1a37505a5c4e116e2e
Kingsley Etukudo <kingsleyj90@gmail.com>
2023-02-14 08:47:29 +0100

    openAPI not working

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,5 +1,32 @@
 <template>
-  <div></div>
+  <div class="flex flex-col flex-1 items-center">
+    <div
+      v-if="route.query.preview"
+      class="text-white bg-weather-secondary w-full text-center">
+      <p>
+        You are curreny previewing this city, click the "+" icon to start
+        tracking this city.
+      </p>
+    </div>
+
+    <div class="flex flex-col items-center text-white py-12">
+      <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+      <p class="text-sm mb-12">
+        {{
+          new Date(weather.currentTime).toLocaleDateString("en-us", {
+            weekday: "short",
+            day: "2-digit",
+            month: "long",
+          })
+        }}
+        {{
+          new Date(weather.currentTime).toLocaleDateString("en-us", {
+            timeStyle: "long",
+          })
+        }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -10,7 +37,7 @@ const route = useRoute();
 
 const getWeatherData = async () => {
   try {
-    const waetherDate =
+    const weatherData =
       await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=0e1e5b2c3e858b575990b4969385ef41&units=imperial
 `);
 
@@ -31,4 +58,5 @@ const getWeatherData = async () => {
 };
 
 const weatherData = await getWeatherData();
+console.log(weatherData);
 </script>
